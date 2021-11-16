0afedfa59345f8eabe3d7ff9f004f781bdb215e6
Blaatand29 <94191792+Blaatand29@users.noreply.github.com>
2021-11-16 16:48:38 +0100

    fixed some stuff

--- logic/utils/paths.py
+++ logic/utils/paths.py
@@ -4,7 +4,7 @@ Paths start with "P_" and then the name of the file
 """
 
 __all__ = [
-    "DIR",
+    "P_DIR",
     "P_DATA",
     "P_CONFIG",
     "P_MAPS",
@@ -22,17 +22,17 @@ from pathlib import Path
 
 # Paths
 
-DIR = Path().parent.parent.parent  # Main directory of the game
+P_DIR = Path().parent.parent.parent  # Main directory of the game
 
-P_DATA = DIR / "data"
+P_DATA = P_DIR / "data"
 P_CONFIG = P_DATA / "config"
 P_MAPS = P_DATA / "maps"
 P_SAVES = P_DATA / "saves"
 
-P_LOGIC = DIR / "logic"
+P_LOGIC = P_DIR / "logic"
 P_UTILS = P_LOGIC / "utils"
 
-P_RESOURCES = DIR / "resources"
+P_RESOURCES = P_DIR / "resources"
 P_FONTS = P_RESOURCES / "fonts"
 P_IMAGES = P_RESOURCES / "images"
 P_SOUNDS = P_RESOURCES / "sounds"
